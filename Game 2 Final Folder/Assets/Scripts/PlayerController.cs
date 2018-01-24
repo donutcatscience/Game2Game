9339fb5d0c1578c97117361d30e9155fba690110
Scanthrax <35387121+Scanthrax@users.noreply.github.com>
2018-01-23 17:09:58 -0700

    Waypoint Navigation for enemy AI
    basic waypoint navigational system

--- Game 2 Final Folder/Assets/Scripts/PlayerController.cs
+++ Game 2 Final Folder/Assets/Scripts/PlayerController.cs
@@ -10,15 +10,22 @@ public class PlayerController : MonoBehaviour {
     Camera cam;  // get reference to the main camera
     [SerializeField] LayerMask movementMask; // reference to Ground layer; set in the inspector
     NavMeshAgent agent; // get reference to the agent; used for navigation
-    int raycastRange = 1000; // range of raycast for click
 
+    //int raycastRange = 1000; // range of raycast for click
+
+    private Transform target; // targets for waypoints
+    private int waypointIndex = 0; // index for which waypoint to travel to
 
     void Start () {
         cam = Camera.main; // obtain camera reference
         agent = GetComponent<NavMeshAgent>(); // obtain agent
+
+        target = Waypoints.points[waypointIndex]; // obtain array of waypoints
     }
 	
 	void Update () {
+        #region left click to move player (TEST)
+        /*
 		if(Input.GetMouseButtonDown(0)) // On left mouse button press
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition); // cast a ray where the user clicked
@@ -29,7 +36,26 @@ public class PlayerController : MonoBehaviour {
                 MoveToPoint(hit.point); // calls a function to move agent
             }
         }
+        */
+        #endregion
+
+        MoveToPoint(target.position); // agent will move to waypoint
+
+        if(Vector3.Distance(transform.position, target.position) <= 0.2f) // if agent has reached close enough to waypoint
+        {
+            GetNextWaypoint(); // go to next waypoint
+        }
 	}
+
+    void GetNextWaypoint()
+    {
+        waypointIndex++; // set index to next waypoint
+        if(waypointIndex >= Waypoints.points.Length) // check if index reaches out of bounds
+        {
+            waypointIndex = 0; // set it back to the beginning
+        }
+        target = Waypoints.points[waypointIndex]; // set the waypoint
+    }
 
     public void MoveToPoint(Vector3 point)
     {
