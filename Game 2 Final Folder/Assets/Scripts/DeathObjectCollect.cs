d8a0d76e787189dc6c58e6b65efcef8a1cd8c5f5
Scanthrax <35387121+Scanthrax@users.noreply.github.com>
2018-01-30 22:55:47 -0700

    object drop on death; collected by nearest unit

---
+++ Game 2 Final Folder/Assets/Scripts/DeathObjectCollect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathObjectCollect : MonoBehaviour {
+
+    GameObject[] nearbyUnits;
+    float range;
+    float tempDistance;
+    float step;
+
+	void Start () {
+        nearbyUnits = GameObject.FindGameObjectsWithTag("Enemy");
+        range = 5f;
+        step = 10f * Time.deltaTime;
+	}
+	
+	void Update () {
+		foreach(GameObject unit in nearbyUnits)
+        {
+            if (unit != null)
+            {
+                tempDistance = Vector3.Distance(transform.position, unit.transform.position);
+                if (tempDistance < range)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, unit.transform.position, step);
+                }
+            }
+        }
+        if (tempDistance < 1f)
+        {
+            print("I have consumed an item that was dropped on death!");
+            Destroy(gameObject);
+        }
+	}
+}
