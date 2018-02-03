972dca9af6528144900a735a3788d3cbf4b2a443
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-02-03 16:04:48 -0700

    Working On Ulitlity Script
    Added:
    Folder for orginization
    Candle and willow units
    Camera Script to follow Candle
    Willow orb that follows Candle

---
+++ Game 2 Final Folder/Assets/Scripts/Player Scripts/CandleMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleMovement : MonoBehaviour {
+    public float speed;
+    private Rigidbody myRigidBody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+
+	}
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        myRigidBody.AddForce(movement * speed);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
