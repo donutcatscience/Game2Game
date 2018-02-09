9d75ed916499300ec2a626bf29785cb4f7a3a321
judai-asu <35709400+judai-asu@users.noreply.github.com>
2018-02-09 06:11:03 -0700

    WASD Movement and Camera
    So far, this is an Isometric-style WASD movement with cardinal and sub-cardinal directions (NE, NW, SE, SW).
    
    Will plan to implement mouse directed movement, but currently in a bind on how to do so.

---
+++ Game 2 Final Folder/Assets/Scripts/CameraAdjuster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAdjuster : MonoBehaviour {
+
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    GameObject Orientator;
+
+    Vector3 forward, right;
+
+	// Use this for initialization
+	void Start ()
+    {
+        forward = Orientator.transform.forward;
+        right.y = 0f;
+        forward = Vector3.Normalize(forward);
+        right = Quaternion.Euler(new Vector3(0, 90, 0)) * forward;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CameraMovement();
+	}
+
+    void CameraMovement()
+    {
+        Vector3 rightMovement = right * speed * Time.deltaTime * Input.GetAxis("HorizontalControl");
+        Vector3 forwardMovement = forward * speed * Time.deltaTime * Input.GetAxis("VerticalControl");
+
+        transform.position += rightMovement;
+        transform.position += forwardMovement;
+    }
+}
