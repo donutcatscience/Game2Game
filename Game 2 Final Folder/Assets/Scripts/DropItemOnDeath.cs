85d477b161ccd885c274566c101050f106615d2c
Ron <35387121+Scanthrax@users.noreply.github.com>
2018-02-12 18:04:52 -0700

    Revert "Pathfinding V2"
    This reverts commit f722d72a513efc89c8a0634a1b5d50607e385874.

--- Game 2 Final Folder/Assets/Scripts/DropItemOnDeath.cs
+++ Game 2 Final Folder/Assets/Scripts/DropItemOnDeath.cs
@@ -19,7 +19,6 @@ public class DropItemOnDeath : MonoBehaviour {
         {
             Instantiate(deathObject, transform.position, Quaternion.identity);
             Destroy(gameObject);
-            
         }
 	}
 }
--- Game 2 Final Folder/Assets/Scripts/Waypoints.cs
+++ Game 2 Final Folder/Assets/Scripts/Waypoints.cs
@@ -2,16 +2,16 @@
 
 public class Waypoints : MonoBehaviour {
 
-    public static GameObject[] points; // array of waypoints set in inspector
+    public static Transform[] points; // array of waypoints set in inspector
 
     public int waypointIndex = 0;
 
     void Awake()
     {
-        points = new GameObject[transform.childCount]; // find the size of the array
+        points = new Transform[transform.childCount]; // find the size of the array
         for (int i = 0; i < points.Length; i++) // instantiate each instance in the array
         {
-            points[i] = transform.GetChild(i).gameObject; // obtain the transforms of the waypoint
+            points[i] = transform.GetChild(i); // obtain the transforms of the waypoint
         }
     }
 }
