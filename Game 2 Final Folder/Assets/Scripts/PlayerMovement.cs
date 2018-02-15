47946bde888c1fcdfffe785df56f4ababdaceada
judai <judaithegamer@gmail.com>
2018-02-14 20:17:03 -0700

    Improved Camera and Movement
    This is the final version of movement and camera.
    
    Movement is still based on axis, but character rotates in accordance to mouse.
    
    This code is heavily based on the Unite 2014 Survival Shooter Tutorial.

---
+++ Game 2 Final Folder/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed = 6f;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turning();
+	}
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0.0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit floorHit;
+
+        if(Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+}
