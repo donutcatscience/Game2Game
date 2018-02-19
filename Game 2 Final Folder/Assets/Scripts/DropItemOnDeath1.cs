b9944df2ed0524226bcd43c8e4af9e14a8ce548f
Donutcat <31439092+donutcatscience@users.noreply.github.com>
2018-02-19 14:47:13 -0700

    Ron Branch Implementation
    All Bugs fixed

---
+++ Game 2 Final Folder/Assets/Scripts/DropItemOnDeath1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemOnDeath : MonoBehaviour {
+
+    bool dead = false;
+    public GameObject deathObject;
+
+
+
+    private void OnMouseDown()
+    {
+        dead = true;
+    }
+
+    void Update () {
+		if(dead)
+        {
+            Instantiate(deathObject, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+	}
+}
