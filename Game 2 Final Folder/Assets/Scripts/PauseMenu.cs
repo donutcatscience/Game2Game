2199c418ac275ee13fa6fe153f2215cceb4aae2f
Cweafer <32285276+Cweafer@users.noreply.github.com>
2018-04-29 11:07:32 -0700

    SaveSystem
    Added a Save and Load System.

---
+++ Game 2 Final Folder/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PauseMenu : MonoBehaviour {
+    public GameObject pauseCanvas;
+    public bool pausetoggle = false;
+    Save file; 
+    // Use this for initialization
+    void Start () {
+        file = new Save();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (pausetoggle)
+            {
+                pausetoggle = false;
+            }
+            else
+            {
+                pausetoggle = true;
+            }
+
+            Cursor.visible = true;
+            pauseCanvas.SetActive(pausetoggle); 
+
+        }
+
+    }
+    public void saveGame()
+    {
+        file.saveIt();
+    }
+    public void LoadGame()
+    {
+        file.loadIt();
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Game 2 Final Folder/Assets/Scripts/Save.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Save : MonoBehaviour {
+    public GameObject pauseCanvas;
+    public bool pausetoggle = false;
+
+    public int gameDayNum = 0;
+    public int resourceCount = 0;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+    }
+    public void saveIt()
+    {
+        Debug.Log("Save");
+        Debug.Log(gameDayNum);
+        Debug.Log(resourceCount);
+        gameDayNum++;
+        resourceCount++;
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.OpenOrCreate);
+
+        gameData game = new gameData();
+        game.gameDay = gameDayNum;
+        game.resourceCounter = resourceCount;
+        bf.Serialize(file, game);
+        file.Close();
+    }
+
+    public void loadIt()
+    {
+        Debug.Log("Load");
+        if(File.Exists(Application.persistentDataPath + "/gameInfo.dat"))
+        {
+            gameDayNum++;
+            resourceCount++;
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/gameInfo.dat", FileMode.Open);
+            gameData game = (gameData)bf.Deserialize(file);
+            file.Close();
+
+            gameDayNum = game.gameDay;
+            resourceCount = game.resourceCounter;
+        }
+    }
+
+    [Serializable]
+    class gameData
+    {
+        public int gameDay;
+        public int resourceCounter;
+    }
+}
